298a0e620b1a2c8396f73a597d7559fe755edde2
Kelly McCuddy <kmccuddy@protonmail.com>
2025-11-22 15:03:13 -0500

    moved all logic out of the while loop for variable scope reasons

--- remote_drive.sh
+++ remote_drive.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 
-storage_server_setup() {
+password_var() {
 	while [ true ]
   do
+      echo "Please enter a password for server user :"
+      read -sp 'Password: ' passvar1
+      echo "Please re-enter the password for server user :"
+      read -sp 'Password: ' passvar2
 
       if [ -z $passvar1 ] || [ -z $passvar2 ]
       then
@@ -14,26 +18,7 @@ storage_server_setup() {
 
       elif [ $passvar1 == $passvar2 ]
       then
-          USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
-          echo "Please enter server address for remote storage:"
-          read -p 'Address: ' server_address
-
-          echo "\nPlease enter user name for server:"
-          read -p 'Username: ' server_user_name
-
-          echo "\nPlease enter a password for $server_user_name :"
-          read -sp 'Password: ' passvar1
-          echo "Please re-enter the password for $server_user_name :"
-          read -sp 'Password: ' passvar2
-
-          sudo sh -c '
-            touch /etc/samba_credentials
-            echo "username=$server_user_name" >> /etc/samba_credentials
-            echo "password=$passvar1" >> /etc/samba_credentials
-            chmod 600 /etc/samba_credentials
-            echo "//$server_address     $USER_HOME/.data     cifs    credentials=/etc/samba_credentials,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,vers=3.0,_netdev,nofail,x-systemd.after=wait-for-ping.service    0 0" >> /etc/fstab
-            mount -t cifs //$server_address $USER_HOME/.data -o credentials=/etc/samba_credentials
-          '
+          echo $password1 >> /etc/samba_credentials
           break
 
       else
@@ -46,7 +31,21 @@ storage_server_setup() {
 }
 
 mkdir $HOME/.data
-storage_server_setup
+USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+echo "Please enter server address for remote storage:"
+read -p 'Address: ' server_address
+
+echo "Please enter user name for server:"
+read -p 'Username: ' server_user_name
+
+sudo sh -c '
+  touch /etc/samba_credentials
+  echo "username=$server_user_name" >> /etc/samba_credentials
+  password_var
+  chmod 600 /etc/samba_credentials
+  echo "//$server_address     $USER_HOME/.data     cifs    credentials=/etc/samba_credentials,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,vers=3.0,_netdev,nofail,x-systemd.after=wait-for-ping.service    0 0" >> /etc/fstab
+  mount -t cifs //$server_address $USER_HOME/.data -o credentials=/etc/samba_credentials
+'
 rm -rf {Documents,Music,Pictures,Videos}
 echo "\nPlease enter the shared directory name:"
 read -p 'Shared directory: ' shared_directory
