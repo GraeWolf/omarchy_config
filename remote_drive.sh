617437d615689153e9409d2e598d3397b6544ea5
Kelly McCuddy <kmccuddy@protonmail.com>
2025-11-21 22:38:34 -0500

    first commit

---
+++ remote_drive.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+
+storage_server_setup() {
+	while [ true ]
+    do
+		echo "Please enter server address for remote storage:"
+		read -sp 'Address: ' server_address
+
+		echo "Please enter user name for server:"
+		read -sp 'Username: ' server_user_name
+
+        echo "Please enter a password for $server_user_name :"
+        read -sp 'Password: ' passvar1
+        echo "Please re-enter the password for $server_user_name :"
+        read -sp 'Password: ' passvar2
+
+
+
+        if [ -z $passvar1 ] || [ -z $passvar2 ]
+        then
+            echo "Password cannot be blank"
+            echo ""
+            echo ""
+            echo ""
+
+        elif [ $passvar1 == $passvar2 ]
+        then
+			sudo touch /etc/samba_credentials
+            sudo echo "username=$server_user_name" >> /etc/samba_credentials
+			sudo echo "password=$passvar1" >> /etc/samba_credentials
+			sudo chmod 600 /etc/samba_credentials
+			sudo echo "//$server_address     $HOME/.data     cifs    credentials=/etc/samba_credentials,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,vers=3.0,_netdev,nofail,x-systemd.after=wait-for-ping.service    0 0" >> /etc/fstab
+			sudo mount -t cifs //$server_address $HOME/.data -o credentials=/etc/samba_credentials
+            break
+
+        else
+            echo "passwords did not match."
+            echo ""
+            echo ""
+            echo ""
+        fi
+    done
+}
+
+mkdir $HOME/.data
+storage_server_setup
+rm -rf {Documents,Music,Pictures,Videos}
+echo "Please enter the shared directory name:"
+read -sp 'Shared directory: ' shared_directory
+
+ln -sf $HOME/.data/$shared_directory/Documents $HOME/Documents
+ln -sf $HOME/.data/$shared_directory/Music $HOME/Music
+ln -sf $HOME/.data/$shared_directory/Pictures $HOME/Pictures
+ln -sf $HOME/.data/$shared_directory/Videos $HOME/Videos
+
