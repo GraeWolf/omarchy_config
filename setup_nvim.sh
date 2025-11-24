51833577f011ecb68d03c5e60469d2d2e7d7753c
Kelly McCuddy <kmccuddy@protonmail.com>
2025-11-23 20:19:05 -0500

    corrected the symlink command

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo "Setting up Nvim"
 if [ -d "$HOME/.config/nvim" ]; then
 	rm -rf "$HOME/.config/nvim"
-	ln -sfn "$HOME/.local/share/omarchy_config/nvim $HOME/.config/nvim"
+	ln -sfn "$HOME/.local/share/omarchy_config/nvim" "$HOME/.config/nvim"
 else
-	ln -sfn "$HOME/.local/share/omarchy_config/nvim $HOME/.config/nvim"
+	ln -sfn "$HOME/.local/share/omarchy_config/nvim" "$HOME/.config/nvim"
 fi
