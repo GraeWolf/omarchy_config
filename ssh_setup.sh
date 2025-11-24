741c253aa5649f8c3b96cdece9aac008d3ed63ea
Kelly McCuddy <kmccuddy@protonmail.com>
2025-11-23 20:14:17 -0500

    first commit

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# Check to see if the .data directory is mounted and the .ssh directory exists
+if [ -d "~/.data/.ssh" ]; then
+	cp -R ".data/.ssh $HOME/"
+	chmod 700 "$HOME/.ssh"
+	chmod 600 "$HOME/.ssh/id_"* "$HOME/.ssh/authorized_keys" "$HOME/.ssh/config"
+	chmod 644 "$HOME/.ssh/"*.pub
+	echo ".ssh has been copied over and permissions set"
+else
+	echo "Directory not found"
+fi
