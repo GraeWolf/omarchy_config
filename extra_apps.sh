e8441ada7dbd349fded025d761d7afc64d468042
Kelly McCuddy <kmccuddy@startmail.com>
2025-11-27 18:08:19 -0500

    changed brave to brave-bin

--- extra_apps.sh
+++ extra_apps.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-extra_apps=(bitwarden calibre pcmanfm kdenlive betterbird brave)
+extra_apps=(bitwarden calibre pcmanfm kdenlive betterbird brave-bin)
 
 for app in "${extra_apps[@]}"; do
 	yay -S --noconfirm --needed $app
