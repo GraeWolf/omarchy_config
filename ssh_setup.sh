2ec0b1af7c12581ab9fef1c97557753b93ab4280
Kelly McCuddy <kmccuddy@startmail.com>
2025-11-27 18:11:20 -0500

    fixed path errors

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 # Check to see if the .data directory is mounted and the .ssh directory exists
-if [ -d "~/.data/.ssh" ]; then
-	cp -R ".data/.ssh" "$HOME/"
+if [ -d "$HOME/.data/.ssh" ]; then
+	cp -R "$HOME/.data/.ssh" "$HOME/"
 	chmod 700 "$HOME/.ssh"
 	chmod 600 "$HOME/.ssh/id_"* "$HOME/.ssh/config"
 	chmod 644 "$HOME/.ssh/"*.pub
